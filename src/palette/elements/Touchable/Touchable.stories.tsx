9b066f06c984ab942dd83bc47e353550dffc8969
Pavlos Vinieratos <pvinis@gmail.com>
2021-12-18 11:48:04 +0200

    getting there

---
+++ src/palette/elements/Touchable/Touchable.stories.tsx
@@ -0,0 +1,31 @@
+import { storiesOf } from "@storybook/react-native"
+import { Text } from "palette"
+import React from "react"
+import { withHooks, withScreenDimensions, withTheme } from "storybook/decorators"
+import { List } from "storybook/helpers"
+import { Touchable } from "."
+import { _test_DisplayState } from "../Button/Button"
+
+storiesOf("Touchable", module)
+  .addDecorator(withTheme)
+  .addDecorator(withScreenDimensions)
+  .addDecorator(withHooks)
+  .add("Presses", () => (
+    <List>
+      <Touchable onPress={() => console.warn("regular")}>
+        <Text>Regular press</Text>
+      </Touchable>
+      <Touchable onPress={() => console.warn("regular")} onLongPress={() => console.warn("long")}>
+        <Text>Regular and Long press</Text>
+      </Touchable>
+      <Touchable
+        onPress={() => console.warn("regular")}
+        onLongPress={[
+          { title: "Action 1", systemIcon: "heart", onPress: () => console.warn("1") },
+          { title: "Action 2", onPress: () => console.warn("2") },
+        ]}
+      >
+        <Text>Regular and Context press</Text>
+      </Touchable>
+    </List>
+  ))
