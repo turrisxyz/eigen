951e929e64923ce0067d56ec6969ceaaa2d259b8
Pavlos Vinieratos <pvinis@gmail.com>
2022-03-18 15:23:09 +0000

    wip

--- .betterer.ts
+++ .betterer.ts
@@ -1,73 +1,49 @@
-import { BettererTest, BettererFileTest } from "@betterer/betterer"
-import lineColumn from "line-column"
-import { smaller } from "@betterer/constraints"
+import { BettererFileTest } from "@betterer/betterer"
 import fs from "fs"
-import glob from "glob"
 
-// const files = glob.sync("**/*")
-// .filter((filename) => !filename.includes("node_modules"))
-// .filter((filename) => !filename.includes("__generated__"))
-// .filter((filename) => !filename.endsWith(".lock"))
-// .filter((filename) => !filename.endsWith(".png"))
-// .filter((filename) => !filename.endsWith(".webp"))
-// .filter((filename) => !filename.endsWith(".jpg"))
-// .filter((filename) => !filename.endsWith(".jar"))
-
-// export const removeEnzyme11 = () => {
-//   const getNumberOfFilesImportingEnzyme = () => {
-//     return files
-//       .map((f) => {
-//         console.log(f)
-//         return f
-//       })
-//       .filter((filename) => filename.includes(".tests.tsx"))
-//       .filter((filename) => {
-//         const content = fs.readFileSync(filename).toString()
-//         return content.includes('from "enzyme"')
-//       }).length
-//   }
-
-//   return new BettererTest({
-//     test: () => getNumberOfFilesImportingEnzyme(),
-//     constraint: smaller,
-//     goal: 0,
-//   })
-// }
-
-export const removeEnzyme = () =>
-  new BettererFileTest(async (filepaths, result) => {
+function removeSpecificString(theString: string) {
+  return new BettererFileTest(async (filepaths, result) => {
     await Promise.all(
       filepaths.map(async (filepath) => {
         const contents = fs.readFileSync(filepath, "utf8").toString()
-        const index = contents.indexOf('from "enzyme"')
+        const index = contents.indexOf(theString)
         if (index !== -1) {
-          const { line, column } = lineColumn(contents, { origin: 0 }).fromIndex(index)
           const file = result.addFile(filepath, contents)
-          file.addIssue(line, column, `lol ${index}`)
+          file.addIssue(
+            index,
+            index + enzymeString.length,
+            "We are trying to migrate away from Enzyme. Try using `renderWithWrappersTL` instead."
+          )
+        }
+      })
+    )
+  })
+}
+
+const enzymeString = 'from "enzyme"'
+export function removeEnzyme() {
+  return new BettererFileTest(async (filepaths, result) => {
+    await Promise.all(
+      filepaths.map(async (filepath) => {
+        const contents = fs.readFileSync(filepath, "utf8").toString()
+        const index = contents.indexOf(enzymeString)
+        if (index !== -1) {
+          const file = result.addFile(filepath, contents)
+          file.addIssue(
+            index,
+            index + enzymeString.length,
+            "We are trying to migrate away from Enzyme. Try using `renderWithWrappersTL` instead."
+          )
         }
       })
     )
   }).include("**/*.tests.tsx")
+}
 
-// export const removeMigrationComments = () => {
-//   const getNumberOfMigrationComments = () => {
-//     return files
-//       .map((f) => {
-//         console.log(f)
-//         return f
-//       })
-//       .filter((filename) => {
-//         console.log(filename)
-//         const content = fs.readFileSync(filename).toString()
-//         return content.includes(
-//           "// @ts-expect-error STRICTNESS_MIGRATION --- 🚨 Unsafe legacy code 🚨 Please delete this and fix any type errors if you have time 🙏"
-//         )
-//       }).length
-//   }
+const migrationString =
+  "// @ts-expect-error STRICTNESS_MIGRATION --- 🚨 Unsafe legacy code 🚨 Please delete this and fix any type errors if you have time 🙏"
+export function removeMigrationComments() {
+  return removeSpecificString(migrationString).include()
+}
 
-//   return new BettererTest({
-//     test: () => getNumberOfMigrationComments(),
-//     constraint: smaller,
-//     goal: 0,
-//   })
-// }
+///circle needs `betterer ci`
--- dangerfile.ts
+++ dangerfile.ts
@@ -37,24 +37,6 @@ See [docs](https://moment.github.io/luxon/api-docs/index.html).
   }
 }
 
-// We are trying to migrate away from Enzyme towards @testing-library/react-native
-const preventUsingEnzyme = () => {
-  const newEnzymeImports = getCreatedFileNames(danger.git.created_files)
-    .filter(testOnlyFilter)
-    .filter((filename) => {
-      const content = fs.readFileSync(filename).toString()
-      return content.includes('from "enzyme"')
-    })
-  if (newEnzymeImports.length > 0) {
-    warn(`We are trying to migrate away from Enzyme towards \`@testing-library/react-native\`, but found Enzyme imports in the following new unit test files:
-
-${newEnzymeImports.map((filename) => `- \`${filename}\``).join("\n")}
-
-See [\`Pill.tests.tsx\`](https://github.com/artsy/eigen/blob/2f32d462bb3b4ce358c8a14e3ed09b42523de8bd/src/palette/elements/Pill/__tests__/Pill-tests.tsx) as an example, or [the docs](https://callstack.github.io/react-native-testing-library/docs/api-queries).
-  `)
-  }
-}
-
 // We are trying to migrate away from test-renderer towards @testing-library/react-native
 const preventUsingTestRenderer = () => {
   const newTRImports = getCreatedFileNames(danger.git.created_files)
@@ -142,7 +124,9 @@ export const validatePRChangelog = () => {
 
   if (res.type === "error") {
     console.log("Something went wrong while parsing the PR description")
-    warn("❌ **An error occurred while validating your changelog, please make sure you provided a valid changelog.**")
+    warn(
+      "❌ **An error occurred while validating your changelog, please make sure you provided a valid changelog.**"
+    )
     return
   }
 
@@ -162,7 +146,9 @@ export const validatePRChangelog = () => {
     "### This PR contains the following changes:\n" +
     Object.entries(pickBy(changedSections, (changesArray) => changesArray.length))
       .map(([section, sectionValue]) => {
-        return `\n- ${changelogTemplateSections[section as keyof typeof changedSections]} (${sectionValue})`
+        return `\n- ${
+          changelogTemplateSections[section as keyof typeof changedSections]
+        } (${sectionValue})`
       })
       .join("")
 
@@ -193,7 +179,6 @@ export const useWebPs = (fileNames: string[]) => {
   const newCreatedFileNames = getCreatedFileNames(danger.git.created_files)
 
   preventUsingMoment()
-  preventUsingEnzyme()
   preventUsingTestRenderer()
   preventUsingRenderRelayTree()
   verifyRemainingDevWork()
